58b61b06d0727ab4dec2d100accb18de18c28490
katafuchix <katafuchix@gmail.com>
2026-01-31 01:15:17 +0900

    Initial Commit

---
+++ SwiftUI_YahooImageSearch_MVVM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_YahooImageSearch_MVVM
+//
+//  Created by cano on 2026/01/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_YahooImageSearch_MVVM/SwiftUI_YahooImageSearch_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_YahooImageSearch_MVVMApp.swift
+//  SwiftUI_YahooImageSearch_MVVM
+//
+//  Created by cano on 2026/01/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_YahooImageSearch_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
